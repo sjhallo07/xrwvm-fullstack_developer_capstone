6101eb6535cf5ced893c8411493c3e3bbeee5aa8
Marcos <abreu760@hotmail.com>
2025-10-11 19:10:05 -0400

    Frontend and backend registration, login, logout, manifest and routing fixes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from django.views.generic import TemplateView
 
 
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path(route='register', view=views.registration, name='register'),
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_user, name='logout'),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     # path for dealer reviews view
     # path for add a review view
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
