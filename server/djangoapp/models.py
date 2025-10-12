794b5b572e13e1aead8d38633c84e81b29967b7a
Marcos <abreu760@hotmail.com>
2025-10-12 02:26:57 -0400

    Update populate.py to add CarMake and CarModel sample data

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,41 +1,34 @@
-
-
-
-
-
-
-
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class CarMake(models.Model):
-	name = models.CharField(max_length=100)
-	description = models.TextField(blank=True)
-	country = models.CharField(max_length=100, blank=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    country = models.CharField(max_length=100, blank=True)
 
-	def __str__(self):
-		return f"{self.name} ({self.country})"
+    def __str__(self):
+        return f"{self.name} ({self.country})"
 
 class CarModel(models.Model):
-	SEDAN = 'Sedan'
-	SUV = 'SUV'
-	WAGON = 'Wagon'
-	COUPE = 'Coupe'
-	HATCHBACK = 'Hatchback'
-	TYPE_CHOICES = [
-		(SEDAN, 'Sedan'),
-		(SUV, 'SUV'),
-		(WAGON, 'Wagon'),
-		(COUPE, 'Coupe'),
-		(HATCHBACK, 'Hatchback'),
-	]
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    COUPE = 'Coupe'
+    HATCHBACK = 'Hatchback'
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe'),
+        (HATCHBACK, 'Hatchback'),
+    ]
 
-	car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models')
-	dealer_id = models.IntegerField()
-	name = models.CharField(max_length=100)
-	type = models.CharField(max_length=20, choices=TYPE_CHOICES)
-	year = models.IntegerField(validators=[MinValueValidator(2015), MaxValueValidator(2023)])
-	color = models.CharField(max_length=50, blank=True)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models')
+    dealer_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    year = models.IntegerField(validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+    color = models.CharField(max_length=50, blank=True)
 
-	def __str__(self):
-		return f"{self.car_make.name} {self.name} ({self.year})"
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
